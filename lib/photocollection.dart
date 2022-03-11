f42d81a2e6ddbcad0e42711d3d83cf3d1be323b2
SubarnaKhadka <subarna31k@gmail.com>
2022-03-11 10:14:46 +0545

    src folder added

--- lib/photocollection.dart
+++ lib/photocollection.dart
@@ -1,7 +1,3 @@
 library photocollection;
 
-
-class photocollection {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/photoview.dart';
---
+++ lib/src/photoview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PhotoView extends StatefulWidget {
+  const PhotoView({Key? key}) : super(key: key);
+
+  @override
+  _PhotoViewState createState() => _PhotoViewState();
+}
+
+class _PhotoViewState extends State<PhotoView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
